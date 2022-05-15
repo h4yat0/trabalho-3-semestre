7702d190e54ac1c6581d4cef19c9fbb7ddd82020
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2022-05-14 21:51:12 -0300

    [ADD] botão para voltar ao inicio

--- index.php
+++ index.php
@@ -98,6 +98,9 @@
 
   <body>
     <div id="page-up"></div>
+    <div id="botao_inicio"> <button id="botao"><svg xmlns="http://www.w3.org/2000/svg"  color="white" width="25" height="25" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">  
+      <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/> </svg></button></a>
+    </div>
     <header>
       <!-- Hero Zone -->
       <div class="hero-image">
@@ -477,6 +480,7 @@
 
     <!-- Custom Script -->
     <script src="./src/script/index.js"></script>
+    <script src="./src/script/voltarInicio.js"></script>
   </body>
 </html>
 
---
+++ src/script/voltarInicio.js
@@ -0,0 +1,5 @@
+var btn = document.querySelector("#botao");
+
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
