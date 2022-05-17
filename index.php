151f524496c0fc1988cc71c684a0b4a72a79c960
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2022-05-16 21:44:21 -0300

    [ADD] adicionando botão para o inicio o inicio (novamente)

--- index.php
+++ index.php
@@ -98,9 +98,9 @@
 
   <body>
     <div id="page-up"></div>
-    <div id="botao_inicio"> <button id="botao"><svg xmlns="http://www.w3.org/2000/svg"  color="white" width="25" height="25" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">  
-      <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/> </svg></button></a>
-    </div>
+     <div id="botao_inicio"> <button id="botao">
+       <svg xmlns="http://www.w3.org/2000/svg"  color="white" width="25" height="25" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">  
+      <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/> </svg></button>
     <header>
       <!-- Hero Zone -->
       <div class="hero-image">
@@ -481,6 +481,11 @@
     <!-- Custom Script -->
     <script src="./src/script/index.js"></script>
     <script src="./src/script/voltarInicio.js"></script>
+
+
+
+
+    
   </body>
 </html>
 
@@ -497,4 +502,23 @@ fwrite($arquivo, "\nDATA | ". $dataFinal."\n\n" );
   
 fwrite($arquivo, "\n=======================//================= \n" );
 fclose($arquivo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
+
+
