1fcbbd19f2c203c2dd7555ffcc530a29aac325c7
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2022-05-26 13:46:33 -0300

    [UPDATE] config update

---
+++ app/config/env/phpN.php
@@ -0,0 +1,7 @@
+<?php
+
+//Diretório base do projeto, se for root, então use apenas /
+define('BASE', '/trabalho-3-semestre/');
+
+//Quantidade de diretórios a ser removido do array de tratamento
+define('REMOVE_INDEX', 0);
