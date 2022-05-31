784f5b72122d7c70cefe74e3f89989f3ac7ba5cb
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2022-05-31 04:55:11 -0300

    [DEL] arquivos desnecessários

--- app/config/env/phpN.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Diretório base do projeto, se for root, então use apenas /
-define('BASE', '/');
-
-//Quantidade de diretórios a ser removido do array de tratamento
-define('REMOVE_INDEX', 0);
