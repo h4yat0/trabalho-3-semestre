e12be2c073395f5ec2b8736632a14689e4cc3669
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2022-05-31 03:42:03 -0300

    Revert "[UPDATE] cada um com sua config"
    This reverts commit 6d5b87f976962a81249df69ff49a5dbe9e04a568.

---
+++ app/config/env/phpN.php
@@ -0,0 +1,7 @@
+<?php
+
+//Diretório base do projeto, se for root, então use apenas /
+define('BASE', '/');
+
+//Quantidade de diretórios a ser removido do array de tratamento
+define('REMOVE_INDEX', 0);
