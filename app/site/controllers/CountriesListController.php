87bb08421bd198935ac6abc7148582a32a73fecf
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2022-06-10 14:33:30 -0300

    [UPDATE] JSON is coming

--- app/site/controllers/CountriesListController.php
+++ app/site/controllers/CountriesListController.php
@@ -10,10 +10,31 @@ class CountriesListController extends Controller
     public function __construct()
     {
     }
+
     public function index()
     {
+        $myJson =  file_get_contents('app/site/models/JSON/countriesList.json', true);
+        $myJsonData = json_decode($myJson, true);
+
+        /* TESTE
+        foreach ($myJsonData as $value) {
+            foreach ($value as $value1) {
+                echo $value1['countryName'] . '<br>';
+                echo $value1['description'] . '<br>';
+
+                foreach ($value1 as $value2)
+                    if (is_array($value2)) {
+                        foreach ($value2 as $value3) {
+                            echo $value3['name'] . '<br>';
+                        }
+                    }
+            }
+        }
+        dd($myJsonData);
+        */
+
         $this->View('countriesList', [
-            'BASE' => BASE
+            "countriesJson" => $myJsonData,
         ]);
     }
 }
